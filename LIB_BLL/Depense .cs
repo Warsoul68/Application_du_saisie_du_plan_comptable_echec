20b11e33f0dcf90e60cd4d8f6adcb8d4b4eac3e6
Mathias Schumacher <guzmer68@gmail.com>
2025-05-08 20:53:57 +0200

    codage des objet recette et depense

---
+++ LIB_BLL/Depense .cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIB_BLL
+{
+    public class Depense
+    {
+        private int idDepense;
+        private string libelleD;
+
+        public Depense(int idDepense, string libelleD)
+        {
+            this.idDepense = idDepense;
+            this.libelleD = libelleD;
+        }
+
+        public int getIdDepense()
+        {
+            return idDepense;
+        }
+
+        public void setIdDepense(int idDepense)
+        {
+            this.idDepense = idDepense;
+        }
+
+        public string getLibelleD()
+        {
+            return libelleD;
+        }
+
+        public void setLibelleD(string libelleD)
+        {
+            this.libelleD = libelleD;
+        }
+    }
+}
---
+++ LIB_BLL/Recette.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIB_BLL
+{
+    public class Recette
+    {
+        private int idRecette;
+        private string libelleR;
+
+        public Recette(int idRecette, string libelleR)
+        {
+            this.idRecette = idRecette;
+            this.libelleR = libelleR;
+        }
+
+        public int getIdRecette()
+        {
+            return idRecette;
+        }
+
+        public void setIdRecette(int idRecette)
+        {
+            this.idRecette = idRecette;
+        }
+
+        public string getLibelleR()
+        {
+            return libelleR;
+        }
+
+        public void setLibelleR(string libelleR)
+        {
+            this.libelleR = libelleR;
+        }
+    }
+}
