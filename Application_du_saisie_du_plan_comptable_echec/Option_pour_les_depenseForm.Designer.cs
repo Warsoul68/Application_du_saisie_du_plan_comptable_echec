31eabcda438323eb3784b1ae0ede632ca4bfeb40
Mathias Schumacher <guzmer68@gmail.com>
2025-05-24 22:05:52 +0200

    codage des différent bouton pour les depense et les compte

--- Application_du_saisie_du_plan_comptable_echec/Option_pour_les_depenseForm.Designer.cs
+++ Application_du_saisie_du_plan_comptable_echec/Option_pour_les_depenseForm.Designer.cs
@@ -29,15 +29,15 @@
         private void InitializeComponent()
         {
             this.lblIdDepense = new System.Windows.Forms.Label();
-            this.cboDepense = new System.Windows.Forms.ComboBox();
             this.lblLibelleDepense = new System.Windows.Forms.Label();
-            this.cboLibelleDepense = new System.Windows.Forms.ComboBox();
             this.dgvDepense = new System.Windows.Forms.DataGridView();
             this.btnRechercher = new System.Windows.Forms.Button();
             this.btnAnnuler = new System.Windows.Forms.Button();
             this.btnUpdate = new System.Windows.Forms.Button();
             this.btnSupprimer = new System.Windows.Forms.Button();
             this.btnAjouter = new System.Windows.Forms.Button();
+            this.txtIdDepense = new System.Windows.Forms.TextBox();
+            this.txtLibelleDepense = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvDepense)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,15 +52,6 @@
             this.lblIdDepense.TabIndex = 0;
             this.lblIdDepense.Text = "Id dépense          :";
             // 
-            // cboDepense
-            // 
-            this.cboDepense.FormattingEnabled = true;
-            this.cboDepense.Location = new System.Drawing.Point(159, 23);
-            this.cboDepense.Margin = new System.Windows.Forms.Padding(4);
-            this.cboDepense.Name = "cboDepense";
-            this.cboDepense.Size = new System.Drawing.Size(72, 24);
-            this.cboDepense.TabIndex = 1;
-            // 
             // lblLibelleDepense
             // 
             this.lblLibelleDepense.AutoSize = true;
@@ -71,15 +62,6 @@
             this.lblLibelleDepense.Size = new System.Drawing.Size(120, 18);
             this.lblLibelleDepense.TabIndex = 2;
             this.lblLibelleDepense.Text = "Libelle Depense :";
-            // 
-            // cboLibelleDepense
-            // 
-            this.cboLibelleDepense.FormattingEnabled = true;
-            this.cboLibelleDepense.Location = new System.Drawing.Point(159, 59);
-            this.cboLibelleDepense.Margin = new System.Windows.Forms.Padding(4);
-            this.cboLibelleDepense.Name = "cboLibelleDepense";
-            this.cboLibelleDepense.Size = new System.Drawing.Size(160, 24);
-            this.cboLibelleDepense.TabIndex = 3;
             // 
             // dgvDepense
             // 
@@ -96,10 +78,11 @@
             this.btnRechercher.Location = new System.Drawing.Point(365, 55);
             this.btnRechercher.Margin = new System.Windows.Forms.Padding(4);
             this.btnRechercher.Name = "btnRechercher";
-            this.btnRechercher.Size = new System.Drawing.Size(100, 28);
+            this.btnRechercher.Size = new System.Drawing.Size(129, 28);
             this.btnRechercher.TabIndex = 5;
-            this.btnRechercher.Text = "Rechercher";
+            this.btnRechercher.Text = "Rechercher par id";
             this.btnRechercher.UseVisualStyleBackColor = true;
+            this.btnRechercher.Click += new System.EventHandler(this.btnRechercher_Click);
             // 
             // btnAnnuler
             // 
@@ -110,6 +93,7 @@
             this.btnAnnuler.TabIndex = 6;
             this.btnAnnuler.Text = "Annuler";
             this.btnAnnuler.UseVisualStyleBackColor = true;
+            this.btnAnnuler.Click += new System.EventHandler(this.btnAnnuler_Click);
             // 
             // btnUpdate
             // 
@@ -120,6 +104,7 @@
             this.btnUpdate.TabIndex = 7;
             this.btnUpdate.Text = "Mettre a jour";
             this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // btnSupprimer
             // 
@@ -130,6 +115,7 @@
             this.btnSupprimer.TabIndex = 8;
             this.btnSupprimer.Text = "Supprimer";
             this.btnSupprimer.UseVisualStyleBackColor = true;
+            this.btnSupprimer.Click += new System.EventHandler(this.btnSupprimer_Click);
             // 
             // btnAjouter
             // 
@@ -140,21 +126,36 @@
             this.btnAjouter.TabIndex = 9;
             this.btnAjouter.Text = "Ajouter";
             this.btnAjouter.UseVisualStyleBackColor = true;
+            this.btnAjouter.Click += new System.EventHandler(this.btnAjouter_Click);
+            // 
+            // txtIdDepense
+            // 
+            this.txtIdDepense.Location = new System.Drawing.Point(165, 23);
+            this.txtIdDepense.Name = "txtIdDepense";
+            this.txtIdDepense.Size = new System.Drawing.Size(54, 22);
+            this.txtIdDepense.TabIndex = 10;
+            // 
+            // txtLibelleDepense
+            // 
+            this.txtLibelleDepense.Location = new System.Drawing.Point(165, 58);
+            this.txtLibelleDepense.Name = "txtLibelleDepense";
+            this.txtLibelleDepense.Size = new System.Drawing.Size(148, 22);
+            this.txtLibelleDepense.TabIndex = 11;
             // 
             // ParametreDepense
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(516, 439);
+            this.Controls.Add(this.txtLibelleDepense);
+            this.Controls.Add(this.txtIdDepense);
             this.Controls.Add(this.btnAjouter);
             this.Controls.Add(this.btnSupprimer);
             this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.btnAnnuler);
             this.Controls.Add(this.btnRechercher);
             this.Controls.Add(this.dgvDepense);
-            this.Controls.Add(this.cboLibelleDepense);
             this.Controls.Add(this.lblLibelleDepense);
-            this.Controls.Add(this.cboDepense);
             this.Controls.Add(this.lblIdDepense);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ParametreDepense";
@@ -169,14 +170,14 @@
         #endregion
 
         private System.Windows.Forms.Label lblIdDepense;
-        private System.Windows.Forms.ComboBox cboDepense;
         private System.Windows.Forms.Label lblLibelleDepense;
-        private System.Windows.Forms.ComboBox cboLibelleDepense;
         private System.Windows.Forms.DataGridView dgvDepense;
         private System.Windows.Forms.Button btnRechercher;
         private System.Windows.Forms.Button btnAnnuler;
         private System.Windows.Forms.Button btnUpdate;
         private System.Windows.Forms.Button btnSupprimer;
         private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.TextBox txtIdDepense;
+        private System.Windows.Forms.TextBox txtLibelleDepense;
     }
 }
--- LIB_DAL/DepenseDAO.cs
+++ LIB_DAL/DepenseDAO.cs
@@ -33,5 +33,96 @@ namespace LIB_DAL
                 return null;
             }
         }
+        public static Depense rechercher(int vIdD)
+        {
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader dr;
+            cmd.Connection = Bdd.getConnexion();
+            cmd.CommandText = "select * from Depense where id = " + vIdD;
+            try
+            {
+                dr = cmd.ExecuteReader();
+                dr.Read();
+
+                Depense d = new Depense(dr.GetInt32(0), dr.GetString(1));
+
+                dr.Close();
+                return d;
+            }
+            catch
+            {
+                return null;
+
+
+            }
+        }
+        public static int creer(Depense d)
+        {
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader dr;
+            cmd.Connection = Bdd.getConnexion();
+            cmd.CommandText = "insert into Depense values (" + d.getIdDepense() + ", '" + d.getLibelleD() + "')";
+            try
+            {
+                dr = cmd.ExecuteReader();
+                dr.Read();
+
+
+                dr.Close();
+                return cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                return 0;
+
+
+            }
+        }
+
+        public static int supprimer(Depense d)
+        {
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader dr;
+            cmd.Connection = Bdd.getConnexion();
+            cmd.CommandText = "delete from Depense where id = " + d.getIdDepense();
+            try
+            {
+                dr = cmd.ExecuteReader();
+                dr.Read();
+
+
+                dr.Close();
+                return cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                return 0;
+
+
+            }
+        }
+
+        public static int modifier(Depense d)
+        {
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader dr;
+            cmd.Connection = Bdd.getConnexion();
+            cmd.CommandText = "update Depense set libelle = '" + d.getLibelleD() + "' where id = " + d.getIdDepense();
+            try
+            {
+                dr = cmd.ExecuteReader();
+                dr.Read();
+
+
+                dr.Close();
+                return cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                return 0;
+
+
+            }
+        }
     }
 }
